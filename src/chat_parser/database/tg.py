69eafc21a83153481587e46ef1d9713bebd668c7
Малека Виктор Фёдорович <viktor.chebotarev@megamarket.ru>
2025-06-11 20:59:15 +0300

    fix

--- src/chat_parser/database/tg.py
+++ src/chat_parser/database/tg.py
@@ -16,15 +16,7 @@ from tenacity import (
     wait_exponential,
 )
 
-# Настраиваем логирование
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-formatter = logging.Formatter(
-    "[%(asctime)s] %(levelname)s %(name)s: %(message)s", "%Y-%m-%d %H:%M:%S"
-)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 
 class ProxySettings(BaseModel):
